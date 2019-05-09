b96d6e1c665e19d59f9780460f401a2d8397f9cf
alex langford <alejandro010795@gmail.com>
2019-05-09 09:55:40 -0600

    alexs commit

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
               <h5>Add your name below</h5>
               <h4>Signed in:</h4>
               <ul>
-                <li>Spencer Smith</li>
+                <li>alex boi</li>
                 {/* add name here*/}
               </ul>
             </div>
