f0d8ea410a832c30922712554e0333a5b201eb37
alex langford <alejandro010795@gmail.com>
2019-05-09 10:40:43 -0600

    message

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ class App extends Component {
               <h5>Add your name below</h5>
               <h4>Signed in:</h4>
               <ul>
+                <li>alex 2</li>
                 <li>alex boi</li>
                 {/* add name here*/}
               </ul>
